72a765dc08c123c0051e1d146ab1c70659dbd7fd
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-07-21 19:12:49 +0200

    containerized gRPC Microservice with docker-compose

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -9,6 +11,9 @@ builder.Services.AddStackExchangeRedisCache(options =>
 });
 // IBasketRepository, BasketRepository dependency injection
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Grpc service dependency injection
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

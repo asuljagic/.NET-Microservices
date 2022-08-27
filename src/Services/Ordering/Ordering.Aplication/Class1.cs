db9efeea408637e25f07ab4ddfb879afe97f1445
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-08-27 15:04:22 +0200

    added abstractions in application layer and defined Email service

--- src/Services/Ordering/Ordering.Aplication/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ordering.Aplication
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Services/Ordering/Ordering.Aplication/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Ordering.Aplication.Mappings
+{
+    //need mapper in order to map domain objects with application objects
+    public class MappingProfile : Profile
+    {
+
+    }
+}
